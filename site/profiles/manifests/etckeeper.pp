0f3b0366538bf4edb577ad9b333352f8ef2d3f66
trino <trino@ekodia.de>
2014-12-17 19:32:38 +0100

    etckeeper now uses git as a default

--- site/profiles/manifests/etckeeper.pp
+++ site/profiles/manifests/etckeeper.pp
@@ -8,4 +8,14 @@ class profiles::etckeeper {
   package { 'etckeeper':
   } -> File <| |>
 
+  file { '/etc/etckeeper/etckeeper.conf':
+    ensure => file,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/profiles/etckeeper.conf',
+  } ->
+  exec { 'etckeeper init':
+    path   => '/usr/local/bin:/usr/bin:/bin:/usr/local/sbin/:/usr/sbin/:/sbin',
+  }
 }
