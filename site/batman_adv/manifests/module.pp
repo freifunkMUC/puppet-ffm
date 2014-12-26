c444586ac82a83e68d62799b5a5378660e9b0cc3
trino <trino@ekodia.de>
2014-12-26 08:01:19 +0100

    removed batman-building-code for non-x86_64-ubuntu

--- site/batman_adv/manifests/module.pp
+++ site/batman_adv/manifests/module.pp
@@ -44,38 +44,7 @@ class batman_adv::module (
             source   => '/tmp/batctl_2014.3.0-1_amd64.deb',
           }
         } else {
-          # If you must, you may build it:
-          #   but its ugly and likely to be broken in some ways
-          $old_version = '2013.4.0'
-
-          package { 'batman-adv-dkms':
-          } ->
-          package { 'batman-adv-source':
-          } ->
-          package { 'batctl':
-            ensure => "${version}-${batctl_minor_version}",
-          } ->
-          exec { "dkms remove batman-adv/${old_version} --all":
-            unless => "test -n \"\$( dkms status -m batman-adv/${old_version} )\"",
-          } ->
-          exec { "git clone http://git.open-mesh.org/batman-adv.git batman-adv-${version}":
-            cwd    => '/usr/src',
-            unless => "stat /usr/src/batman-adv-${version}",
-          } ->
-          exec { "git checkout v${version}":
-            cwd => "/usr/src/batman-adv-${version}",
-          } ->
-          file { "/usr/src/batman-adv-${version}/dkms.conf":
-            ensure  => file,
-            owner   => 'root',
-            group   => 'root',
-            mode    => '0644',
-            content => template('batman_adv/dkms.conf.erb'),
-          } ->
-          exec { "dkms --force install batman-adv/${version}":
-          } ->
-          kmod::load { 'batman_adv':
-          }
+          fail("${::operatingsystem} ${::operatingsystemmajrelease} on ${::hardwaremodel} is not supported!")
         }
       } else {
         fail("${::operatingsystem} ${::operatingsystemmajrelease} is not yet supported!")
