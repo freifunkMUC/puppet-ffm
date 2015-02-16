8d1b20db31098878b77fc67b448da4f159989a29
mephisto <mephisto@mephis.to>
2015-02-16 14:28:11 +0100

    Added bandwidth Parameter

--- site/profile/manifests/batman_adv.pp
+++ site/profile/manifests/batman_adv.pp
@@ -4,6 +4,8 @@ class profile::batman_adv (
   $gateway_number,
   $netmask,
   $version = '2014.3',
+  $txbw = '10MBit',
+  $rwbw = '10MBit',
 ) {
 
   include ::profile::networking
@@ -19,6 +21,8 @@ class profile::batman_adv (
     gateway_number             => $gateway_number,
     gateway_ip                 => $gateway_ip,
     netmask                    => $netmask,
+    txbw                       => $txbw,
+    rxbw                       => $rxbw,
     vpn_routing_table          => $::profile::networking::vpn_routing_table_name,
     ipv6_prefix_without_length => $::profile::radvd::ipv6_prefix_without_length,
   }
