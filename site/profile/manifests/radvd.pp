16e0723a391069c556ef408f32fe5e48aa74566c
Franz Pletz <fpletz@fnordicwalking.de>
2015-02-28 22:23:39 +0100

    Add option whether to announce an ipv6 router with radvd

--- site/profile/manifests/radvd.pp
+++ site/profile/manifests/radvd.pp
@@ -1,25 +1,30 @@
 class profile::radvd (
   $ipv6_prefix_without_length,
   $ipv6_prefix_length,
+  $announce_router=false,
 ) {
 
   include ::profile::batman_adv
 
   contain ::radvd
 
-  $hex_gateway_number = inline_template("<%= ${::radvd::gateway_number}.to_i.to_s(16) -%>")
-  $rdnss              = "${::radvd::ipv6_prefix_without_length}${hex_gateway_number}"
+  $hex_gateway_number = inline_template("<%= ${::profile::batman_adv::gateway_number}.to_i.to_s(16) -%>")
+  $rdnss              = "${ipv6_prefix_without_length}${hex_gateway_number}"
   $ipv6_prefix        = "${ipv6_prefix_without_length}/${ipv6_prefix_length}"
 
   ::radvd::interface { $::profile::batman_adv::bridge:
     options => {
       'AdvSendAdvert'      => 'on',
       'MaxRtrAdvInterval'  => 100,
+      'AdvDefaultLifetime' => $announce_router ? {
+        true  => 100,
+        false => 0,
+      },
     },
     prefixes => {
       "${ipv6_prefix}" => {},
     },
-    rndss => {
+    rdnss => {
       "${rdnss}" => {}
     }
   }
