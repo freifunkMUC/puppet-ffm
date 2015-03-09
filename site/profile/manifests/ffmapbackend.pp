96f1a65d3a5504b001d400d159766356248cd170
Jonas <jonas@ekodia.de>
2015-03-10 00:05:03 +0100

    - fix dir rights

--- site/profile/manifests/ffmapbackend.pp
+++ site/profile/manifests/ffmapbackend.pp
@@ -4,10 +4,8 @@
 class profile::ffmapbackend (
   $domain          = $::fqdn,
   $git_repo_url    = 'https://github.com/freifunkMUC/ffmap-backend',
-  $git_revision = 'bat15',
-  $git_destination = '/opt/ffmap-backend',
-) {
-
+  $git_revision    = 'bat15',
+  $git_destination = '/opt/ffmap-backend',) {
   $peers_directory = "${::fastd::config_path}/peers"
   $mesh_network_interface = hiera('profile::batman_adv::bridge')
   $ffmap_user = 'ffmap'
@@ -18,18 +16,24 @@ class profile::ffmapbackend (
   include ::package::git
 
   $destination_directory = "/var/www/${domain}"
-  
+
+  file { $destination_directory:
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
+  }
+
   user { 'ffmap':
-    home => $git_destination,
+    home   => $git_destination,
     groups => ['www-data']
   }
 
   file { '/etc/sudoers.d/ffmap_batctl':
-    mode => '0440',
-    owner => 'root',
-    group => 'root',
+    mode    => '0440',
+    owner   => 'root',
+    group   => 'root',
     content => "ffmap ALL = NOPASSWD:/usr/local/sbin/batctl\n"
-   }
+  }
 
   vcsrepo { $git_destination:
     ensure   => latest,
@@ -40,13 +44,12 @@ class profile::ffmapbackend (
     require  => Package['git'],
     revision => $git_revision,
     notify   => Exec['generate_nodesjson'],
-  }
-  ->
+  } ->
   file { "${git_destination}/mkmap.sh":
     ensure  => file,
     content => template("${module_name}/ffmapbackend/mkmap.sh.erb"),
     mode    => '0555',
-    owner    => 'ffmap',
+    owner   => 'ffmap',
   }
 
   $ffmap_command = "${git_destination}/mkmap.sh "
@@ -54,12 +57,14 @@ class profile::ffmapbackend (
   exec { 'generate_nodesjson':
     command     => $ffmap_command,
     refreshonly => true,
-    user        => 'ffmap'    
+    user        => 'ffmap',
+    require     => File[$destination_directory],
   }
 
   cron { 'cron_nodesjson':
     command => $ffmap_command,
     minute  => '*/1',
-    user    => 'ffmap'
+    user    => 'ffmap',
+    require     => File[$destination_directory],
   }
 }
