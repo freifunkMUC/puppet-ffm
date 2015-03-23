edc062137e2898ac99b012ce992b491c068ad67c
Franz Pletz <fpletz@fnordicwalking.de>
2015-03-23 22:45:44 +0100

    Set fq_codel as default qdisc

--- site/profile/manifests/networking.pp
+++ site/profile/manifests/networking.pp
@@ -3,21 +3,11 @@ class profile::networking (
   $vpn_routing_table_nr = '42',
 ) {
 
-  sysctl {
-    'net.ipv4.ip_forward': value => '1',
-  }
-
-  sysctl {
-    'net.ipv6.conf.all.forwarding': value => '1',
-  }
-
-  sysctl {
-    'net.ipv4.conf.default.rp_filter': value => '2',
-  }
-
-  sysctl {
-    'net.ipv4.conf.all.rp_filter': value => '2',
-  }
+  sysctl { 'net.ipv4.ip_forward': value => '1' }
+  sysctl { 'net.ipv6.conf.all.forwarding': value => '1' }
+  sysctl { 'net.ipv4.conf.default.rp_filter': value => '2' }
+  sysctl { 'net.ipv4.conf.all.rp_filter': value => '2' }
+  sysctl { 'net.core.default_qdisc': value => 'fq_codel' }
 
   augeas { 'routing table':
     context => '/files/etc/iproute2/rt_tables',
