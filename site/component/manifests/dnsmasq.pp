740dc045023aae791ec391196688ead83caad2cb
Franz Pletz <fpletz@fnordicwalking.de>
2015-09-21 03:48:02 +0200

    Finish dnsmasq config

--- site/component/manifests/dnsmasq.pp
+++ site/component/manifests/dnsmasq.pp
@@ -1,7 +1,9 @@
 class component::dnsmasq (
-  $pools = {}
+  $pools = {},
+  $dnsservers = {}
 ) {
   contain ::dnsmasq
 
   create_resources('dnsmasq::dhcp', $pools)
+  create_resources('dnsmasq::dnsserver', $dnsservers)
 }
