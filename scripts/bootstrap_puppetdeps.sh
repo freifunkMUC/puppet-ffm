ae8c08943e521658859914e3ebd8ad289818e3a1
Franz Pletz <fpletz@fnordicwalking.de>
2015-09-25 03:21:48 +0200

    Install puppet modules with r10k while bootstrapping

--- scripts/bootstrap_puppetdeps.sh
+++ scripts/bootstrap_puppetdeps.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-STAMP="/var/lib/.puppet_bootstrapped"
+set -e
 
-if [ ! -e $STAMP ]; then
+STAMP="/var/lib/.puppet_bootstrapped"
+MOD_STAMP="/vagrant/modules/.stamp"
+
+if [ ! -e "$STAMP" ]; then
     curl -Oq https://apt.puppetlabs.com/puppetlabs-release-stable.deb
     dpkg -i puppetlabs-release-stable.deb
     rm puppetlabs-release-stable.deb
 
     apt-get update
-    apt-get upgrade
-
-    apt-get -y install git puppet facter librarian-puppet
+    apt-get -y upgrade
+    apt-get -y install git puppet facter r10k
 
     touch "$STAMP"
 fi
+
+if [ ! -e "$MOD_STAMP" ] || [ "$MOD_STAMP" -ot /vagrant/Puppetfile ]; then
+    cd /vagrant
+
+    r10k -v info puppetfile install
+
+    touch "$MOD_STAMP"
+fi
+
