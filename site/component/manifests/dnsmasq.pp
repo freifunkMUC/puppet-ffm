606aaf9981ce35a5382acf916686d09d34886ffb
Franz Pletz <fpletz@fnordicwalking.de>
2015-10-04 18:50:04 +0200

    Deep-merge dnsmasq::config_hash and increase max lease limit

--- site/component/manifests/dnsmasq.pp
+++ site/component/manifests/dnsmasq.pp
@@ -1,7 +1,11 @@
 class component::dnsmasq (
   $pools = {},
-  $dnsservers = {}
+  $dnsservers = {},
 ) {
+  class { '::dnsmasq':
+    config_hash => hiera_hash('dnsmasq::config_hash', {}),
+  }
+
   contain ::dnsmasq
 
   create_resources('dnsmasq::dhcp', $pools)
